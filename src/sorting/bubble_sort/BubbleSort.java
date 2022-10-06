eb05cc954fcac78d86a3bafd59adf1c087327594
Ahmed <akhaled.vi@gmail.com>
2022-10-07 00:34:15 +0200

    Added bubble sort algorithm

---
+++ src/sorting/bubble_sort/BubbleSort.java
@@ -0,0 +1,20 @@
+package sorting.bubble_sort;
+
+public class BubbleSort {
+    static void bubbleSort(int[] arr) {
+        int length = arr.length;
+        int temp;
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 1; j < (length - i); j++) {
+                if (arr[j - 1] > arr[j]) {
+                    //swap elements
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+    }
+}
